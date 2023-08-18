1869ec1e5e3448a20ced79b7f62e017af72dc1fc
Manson Chen <5242212+mansonchen29@users.noreply.github.com>
2023-08-18 11:39:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
